91f7ef127191923e18dd43477c622df2252d3b37
Kevyn Gandaho <kgandaho@gmail.com>
2019-06-05 23:11:26 -0500

    Finished TechJobs MVC

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,8 +4,10 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.PipedOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,7 +16,7 @@ import java.util.HashMap;
  */
 @Controller
 @RequestMapping("search")
-public class SearchController {
+public class SearchController extends JobData {
 
     @RequestMapping(value = "")
     public String search(Model model) {
@@ -24,4 +26,23 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value = "results")
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Search By Type");
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
+
+    }
+
 }
+
+
+
