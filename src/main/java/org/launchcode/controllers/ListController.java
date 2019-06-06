02d905c5c139b695ca004ad46e2df1f575048b0a
Kevyn Gandaho <kgandaho@gmail.com>
2019-06-06 13:17:08 -0500

    Fixed views, added job count

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -41,12 +41,15 @@ public class ListController {
             ArrayList<HashMap<String, String>> jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
             model.addAttribute("jobs", jobs);
+            int numJobs = jobs.size();
+            model.addAttribute("numJobs",numJobs);
             return "list-jobs";
         } else {
             ArrayList<String> items = JobData.findAll(column);
             model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
             model.addAttribute("column", column);
             model.addAttribute("items", items);
+
             return "list-column";
         }
 
@@ -59,6 +62,8 @@ public class ListController {
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
         model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
         model.addAttribute("jobs", jobs);
+        int numJobs = jobs.size();
+        model.addAttribute("numJobs",numJobs);
 
         return "list-jobs";
     }
